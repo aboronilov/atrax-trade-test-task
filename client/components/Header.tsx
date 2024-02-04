7296d2dc697ae1cd21f0dd8f349ef990476e5f15
Anatoly Boronilov <boronilov@gmail.com>
2024-02-04 12:10:02 +0200

    readme updated

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = (props: Props) => {
     return (
         <header className='flex items-center justify-between px-8 py-5'>
             <Link href="https://www.atrucks.su/" target='_blank' className='flex items-center cursor-pointer'>
-                <div className='bg-red-300 dark:bg-black w-fit'>
+                <div className='bg-red-700 dark:bg-black w-fit'>
                     <Image
                         src='https://www.atrucks.su/static/images/logo.3f0bed1d838c.png'
                         alt='logo'
